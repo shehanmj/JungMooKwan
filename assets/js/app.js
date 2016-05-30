5616a53241a16f9066b68b1c131448c7ddcb20f8
Michael Jason Shehan <shehanmj@yahoo.com>
2016-05-30 08:22:35 +0000

    split index file into separate files for better organization. Preparing to convert project to Angularjs.

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('jmkController', ['$scope', function($scope) {
+  $scope.greeting = 'We aim to enhance each student’s physical, emotional, intellectual, social, and character development by teaching real,self-defense oriented martial arts, valuable life skills, and guiding principles that are essential to a healthy and well-balanced lifestyle. Our goal is to help each student become more than they thought they could become, and to help them always be ready for the challenges of life.';
+}]);
